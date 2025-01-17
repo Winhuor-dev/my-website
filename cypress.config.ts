20b92609d30a2f22877e137f572d17f0f02f61b1
HAN WINHUOR <winhuorhan@gmail.com>
2025-01-17 14:24:54 +0700

    first commit

---
+++ cypress.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from "cypress";
+import fs from "fs";
+
+export default defineConfig({
+  video: true,
+  screenshotsFolder: "cypress/screenshots", // Specify folder for screenshots
+  videosFolder: "cypress/videos", // Specify folder for videos
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    // specPattern: "cypress/e2e/**/*.cy.ts",
+    setupNodeEvents(on, config) {
+      on(
+        "after:spec",
+        (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+          if (results && results.video) {
+            // Do we have failures for any retry attempts?
+            const failures = results.tests.some((test) =>
+              test.attempts.some((attempt) => attempt.state === "failed")
+            );
+            if (!failures) {
+              // delete the video if the spec passed and no tests retried
+              fs.unlinkSync(results.video);
+            }
+          }
+        }
+      );
+    }
+  }
+});
